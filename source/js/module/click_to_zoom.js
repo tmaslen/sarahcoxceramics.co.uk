57a6e5c50c33322cd6383480cb19cf20dedae87e
maslen <maslet02@jake.lan>
2014-05-02 19:19:50 +0100

    improved click to zoom

--- source/js/module/click_to_zoom.js
+++ source/js/module/click_to_zoom.js
@@ -2,7 +2,7 @@
 
 	var ClickToZoom = function () {
 		
-		this.elm = $('<div class="click-to-zoom__modal hide">' +
+		this.elm = $('<div class="click-to-zoom__modal hide--modal">' +
 			'<a href="#" class="click-to-zoom__close-button"><span>&times;</span></a>' +
 			'<div class="click-to-zoom__img-holder image--stretch"></div>' +
 		'</div>');
@@ -11,7 +11,7 @@
 
 		var clickToZoom = this;
 		this.elm.find('.click-to-zoom__close-button').on('click', function (e) {
-			clickToZoom.elm.addClass('hide');
+			clickToZoom.elm.addClass('hide--modal');
 			if (clickToZoom.elmToReturnTo) {
 				clickToZoom.elmToReturnTo.focus();
 			}
@@ -20,8 +20,25 @@
 		});
 
 		this.showImage = function (imgElm) {
-			this.elm.removeClass('hide');
-			this.imgHolder.html('<img src="' + imgElm.src + '" class="js-image_replace click-to-zoom__no" /><div class="image-caption">"' + imgElm.alt + '"</div>');
+			this.elm.removeClass('hide--modal');
+			this.imgHolder.html('<img src="' + imgElm.src + '" class="js-image_replace click-to-zoom__no" /><div class="image-caption image-caption--click-to-zoom">"' + imgElm.alt + '"</div>');
+			if ($(imgElm).hasClass('portrait')) {
+				this.imgHolder.addClass('click-to-zoom__img-holder--portrait');
+				this.imgHolder.removeClass('click-to-zoom__img-holder--landscape');
+				this.elm.height(window.innerHeight);
+				var imageWidth = window.innerHeight*0.75;
+				if (window.innerWidth >= 736) {
+					this.imgHolder.css('width', imageWidth+'px');
+				}
+				else {
+					this.imgHolder.css('width', '');
+				}
+			}
+			else {
+				this.imgHolder.addClass('click-to-zoom__img-holder--landscape');
+				this.imgHolder.removeClass('click-to-zoom__img-holder--portrait');
+				this.imgHolder.css('width', '');
+			}
 			this.elm.find('.click-to-zoom__close-button')[0].focus();
 			this.elmToReturnTo = imgElm;
 			if (pubsub) {
